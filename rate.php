686c1032c8eeca2e2c0a376b8a484ba2700d4779
Harry Yunanto <yunanto2209@gmail.com>
2013-05-08 05:59:26 +0700

    ambil kurs dari bank indonesia

---
+++ rate.php
@@ -0,0 +1,26 @@
+<?php
+require('./phpQuery/phpQuery.php');
+
+// $html = @file_get_contents('http://www.bi.go.id/web/en/Moneter/Kurs+Bank+Indonesia/Kurs+Transaksi/');
+$html = @file_get_contents('http://localhost/rate/tes.html');
+
+phpQuery::newDocumentHTML($html, $charset = 'utf-8'); 
+$tables = pq('table[bgcolor="#cccccc"]');
+$data = array();
+
+foreach ($tables as $table){
+	$i = 0;
+  	foreach(pq($table)->find('tr') as $tr) {
+  		if($i > 1) {
+	  		$d = array();
+	  		foreach (pq($tr)->find('td') as $td) {
+	  			$d[] = pq($td)->text();
+	  		}
+	  		$data[] = array('kurs' => $d);
+	  	}
+  		$i++;
+	}
+}
+phpQuery::unloadDocuments();
+echo "<pre>";print_r($data);echo "</pre>";die;
+?>
